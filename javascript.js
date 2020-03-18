847a504186c2d9af2726a2ddc958776646eb934f
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2020-03-18 13:16:03 +0100

    Lite JS

---
+++ javascript.js
@@ -0,0 +1,11 @@
+/**
+ * Detta är javaScript skapat av Tobias Lindberg 2020-03-16
+ * 
+ */
+
+// Nu ska jag skapa en funktion.
+
+function visaDialogruta(){
+    alert('Den här koden körs från egen fil.');
+}
+
---
+++ js-exempel.js
@@ -0,0 +1,9 @@
+
+function VisaDialog(){
+    //En kommentar på en rad.
+    alert("Hej Tobias!");
+    /* En 
+    kommentar som 
+    sträcker sig över 
+    flera rader. */
+}
